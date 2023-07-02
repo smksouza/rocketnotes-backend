676738f357612ee43bc92c1e0c02eb828d3cf967
Samuel Souza <smksouza.dev@gmail.com>
2023-07-02 20:32:55 -0300

    validação de usuario por email e senha

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const { Router } = require("express");
 const usersRouter = require("./users.routes");
 const notesRouter = require("./notes.routes");
 const tagsRouter = require("./tags.routes");
+const sessionsRouter = require("./sessions.routes");
 
 
 const routes = Router();
+routes.use("/sessions", sessionsRouter);
 routes.use("/users", usersRouter);
 routes.use("/notes", notesRouter);
 routes.use("/tags", tagsRouter);
